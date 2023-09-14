959be8661e1e1ce8db3c77ddede8a775e50fe2db
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-09-14 06:53:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+  let bin=new Array();
+	let i=0;
+	while(num>0){
+		bin[i]=num%2;
+		num=Math.floor(num/2);
+		i++;
+	}
+	for (let j = i - 1; j >= 0; j--)
+        document.write(bin[j]);
 }
 
 window.decimalToBinary = decimalToBinary;
